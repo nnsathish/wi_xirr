0de7d997a6bb4189135ff358b138ce81d4540785
Shaunak Dey <shaunak.dey@weinvest.net>
2020-04-01 11:02:29 +0530

    Update wi_xirr_spec.rb

--- spec/lib/wi_xirr_spec.rb
+++ spec/lib/wi_xirr_spec.rb
@@ -5,7 +5,7 @@ describe WiXirr do
     let(:amounts) { [1000, -600, -6000] }
     let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
     it 'should calculate the XIRR' do
-      result = WiXirr.calculate(amounts, dates, 0)
+      result = WiXirr.calculate(amounts, dates, 0, 100)
       expect(result).to be_within(0.00001).of(0.225683)
     end
   end
@@ -14,7 +14,7 @@ describe WiXirr do
     let(:amounts) { [1000, -600, -200] }
     let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
     it 'should calculate the XIRR' do
-      result = WiXirr.calculate(amounts, dates, 0)
+      result = WiXirr.calculate(amounts, dates, 0, 100)
       expect(result).to be_within(0.00001).of(-0.0346)
     end
   end
@@ -23,7 +23,7 @@ describe WiXirr do
     let(:amounts) { [-200, -600, 1000] }
     let(:dates) { [Time.parse("1995-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1985-01-01").to_f] }
     it 'should calculate the XIRR' do
-      result = WiXirr.calculate(amounts, dates, 0)
+      result = WiXirr.calculate(amounts, dates, 0, 100)
       expect(result).to be_within(0.00001).of(-0.0346)
     end
   end
@@ -33,7 +33,7 @@ describe WiXirr do
       let(:amounts) { [-1000, -600, -6000] }
       let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -42,7 +42,7 @@ describe WiXirr do
       let(:amounts) { [1000, 600, 6000] }
       let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -51,7 +51,7 @@ describe WiXirr do
       let(:amounts) { [1000, -600, -600] }
       let(:dates) { [Time.parse("2020-01-01").to_f, Time.parse("2020-01-01").to_f, Time.parse("2020-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -60,7 +60,7 @@ describe WiXirr do
       let(:amounts) { [0, 0, 0] }
       let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -69,7 +69,7 @@ describe WiXirr do
       let(:amounts) { [1000, -600, 500] }
       let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -78,7 +78,7 @@ describe WiXirr do
       let(:amounts) { [1000, -600, -500] }
       let(:dates) { [Time.parse("1985-01-01").to_f, Time.parse("1990-01-01").to_f, Time.parse("1995-01-01").to_f] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, -1)
+        result = WiXirr.calculate(amounts, dates, -1, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -87,7 +87,7 @@ describe WiXirr do
       let(:amounts) { [] }
       let(:dates) { [] }
       it 'should return -1' do
-        result = WiXirr.calculate(amounts, dates, -1)
+        result = WiXirr.calculate(amounts, dates, -1, 100)
         expect(result).to be(-1.0)
       end
     end
@@ -98,7 +98,7 @@ describe WiXirr do
       let(:amounts) { [10000, -305.6, -500] }
       let(:dates) { [Time.parse("2014-04-15").to_f, Time.parse("2014-5-15").to_f, Time.parse("2014-10-19").to_f] }
       it 'should return XIRR matching with Excel' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be_within(0.00001).of(-0.99681)
       end
     end
@@ -107,7 +107,7 @@ describe WiXirr do
       let(:amounts) { [900, -13.5] }
       let(:dates) { [Time.parse("2014-11-07").to_f, Time.parse("2015-05-06").to_f] }
       it 'should return XIRR matching with Excel' do
-        result = WiXirr.calculate(amounts, dates, 0)
+        result = WiXirr.calculate(amounts, dates, 0, 100)
         expect(result).to be_within(0.00001).of(-0.99979)
       end
     end
@@ -117,7 +117,7 @@ describe WiXirr do
     let(:amounts) { [-1293.6, 1990, -1590, 1990] }
     let(:dates) { [1549955890, 1547104687.0, 1547191087.0, 1549955888.0] }
     it 'should return -1 instead of going in a infinte loop' do
-      result = WiXirr.calculate(amounts, dates, 0)
+      result = WiXirr.calculate(amounts, dates, 0, 100)
       expect(result).to eq(-1)
     end
   end
@@ -176,7 +176,7 @@ describe WiXirr do
         amounts << d[0]
         dates << d[1]
       end
-      benches = Benchmark.measure { (1..100000).each { |i| WiXirr.calculate(amounts, dates, 0) }}
+      benches = Benchmark.measure { (1..100000).each { |i| WiXirr.calculate(amounts, dates, 0, 100) }}
       expect(benches.total).to be_within(0.2).of(0.6)
     end
   end
